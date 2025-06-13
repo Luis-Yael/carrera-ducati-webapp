2e13802ef1ccc02c0fc67fdab8ef4220643c0b48
Luis Yael Mendez <luisyaelms@gmail.com>
2025-06-13 14:54:09 -0600

    Login de la app web móvil
    Se agrega el proyecto completo del fronted para la app carrera-ducati.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular material
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegistroScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.scss']
+})
+export class LoginScreenComponent implements OnInit{
+
+  public errors:any = {};
+  public username: string = "";
+  public password: string = "";
+  //Para la contraseña
+  public hide_1: boolean = false;
+  public inputType_1: string = 'password';
+
+  constructor(
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+
+  }
+
+  public login(){
+
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro"]);
+  }
+
+   public recuperarPwd(){
+
+  }
+
+  showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  //Función para verificar si se habilita la pantalla mobile
+  public isMobile(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(navigator.userAgent)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+}
